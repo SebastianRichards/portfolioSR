69b6400a6dd36f8a52a515cd67617d52b15383c4
Sebastian Richards <sebastianrichards@me.com>
2022-04-18 23:36:53 +0100

    fixed django_on_heroku bug

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
-import django_heroku
+import django_on_heroku
 import dj_database_url
 from decouple import config
 
@@ -147,4 +147,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
